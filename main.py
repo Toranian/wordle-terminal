428a12ba7a940bf83fad4d4798ec654a26b82f85
Isaac Morrow <isaacmorrowofficial@gmail.com>
2022-02-22 12:15:03 -0800

    Fixed a few bugs.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import sys
+import sys, os
+from turtle import clear
 from colorama import init
 from termcolor import colored
 from random import choice
 from time import sleep
 init()
 
+
+
+cant_use = []
 print("Welcome to Wordle!")
 def load_words(filename: str) -> list:
 
-	with open(filename, 'r') as f:
+	full_path = os.getcwd() + "/" + filename
+	with open(full_path, 'r') as f:
 		return [line.strip() for line in f]
 
 def pick_word(words: list) -> str:
@@ -24,6 +29,9 @@ def calc_word(word, goal_word):
 			print("[" + colored(letter, 'yellow') + "]", end='')
 		else:
 			print("[" + colored(letter, 'white') + "]", end='')
+
+			if letter not in cant_use:
+				cant_use.append(letter)
 		sleep(0.1)
 	print()
 
@@ -33,6 +41,9 @@ def print_words(word_bank, goal_word, attempts):
 		calc_word(word, goal_word)
 	print("[] [] [] [] []\n" * attempts)
 
+def clear_screen():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
 def main():
 	all_words = load_words('words.txt')
 	goal_word = pick_word(all_words)
@@ -40,13 +51,19 @@ def main():
 	attempts = 6
 
 	while attempts > 0:
-
 		print_words(word_bank, goal_word, attempts)
 		
+		if len(cant_use) > 0:
+			print("You can't use:", end=' ')
+			for letter in cant_use:
+				print(letter + ", ", end='')
+			print()
 		in_word = input("Enter a word: ")
 
 		if in_word == goal_word:
-			print("You beat the game in only {} attempts!".format(len(word_bank+1)))
+			word_bank.append(in_word)
+			print_words(word_bank, goal_word, attempts)
+			print(f"You beat the game in only {6-attempts} attempts!")
 			sys.exit(0)
 		
 		elif in_word == "quit" or in_word == "exit":
@@ -65,7 +82,8 @@ def main():
 			attempts -= 1
 			print("You have", attempts, "attempts left.")
 		
-		if attempts == 0:
-			print(f"You lost! The word was {goal_word}")
-			sys.exit(0)
+		clear_screen()
+		
+	print(f"\nYou lost! The word was {goal_word}")
+
 main()
